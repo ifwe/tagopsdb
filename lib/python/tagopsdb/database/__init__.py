c9d5663adbfc38c6829139dc572e24c729ad19da
Kenneth Lareau <klareau@tagged.com>
2012-06-19 19:13:10 -0700

    Generate connection string via passed information and configuration file

--- lib/python/tagopsdb/database/__init__.py
+++ lib/python/tagopsdb/database/__init__.py
@@ -1,20 +1,42 @@
+import ConfigParser
+
 from sqlalchemy import create_engine
 
 from tagopsdb.database.meta import Base, Session
 #from tagopsdb.database.test_db import Testing
 from tagopsdb.database.model import *
 
-def init_session():
+
+def create_dbconn_string(db_user, db_password):
+    """Create the connection string for the database"""
+
+    with open('/etc/tagops/tagopsdb.conf') as conf_file:
+        config = ConfigParser.SafeConfigParser()
+        config.readfp(conf_file)
+
+    db_host = config.get('db', 'hostname')
+    db_name = config.get('db', 'db_name')
+
+    return 'mysql+oursql://%s:%s@%s/%s' % (db_user, db_password,
+                                           db_host, db_name)
+
+
+def init_session(db_user, db_password):
     #engine = create_engine('sqlite:///testing.db', echo=True)
-    engine = create_engine('mysql+oursql://tagopsdb_writer:bhagbhag@localhost'
-                           '/TagOpsDB2', echo=True)
+    debug = True
+    dbconn_string = create_dbconn_string(db_user, db_password)
+    engine = create_engine(dbconn_string, echo=debug)
+
     Session.configure(bind=engine)
+
 
 def create_tables():
     Base.metadata.create_all(bind=Session.bind)
 
+
 def drop_tables():
     Base.metadata.drop_all(bind=Session.bind)
+
 
 def init_database():
     drop_tables()
