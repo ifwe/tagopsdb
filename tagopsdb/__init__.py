1eba22f12c6802c9a9ce71db2533ef81ebf8d448
Mike Dougherty <mdougherty@tagged.com>
2014-05-20 14:52:53 -0700

    Some stuff to ease testing

--- tagopsdb/__init__.py
+++ tagopsdb/__init__.py
@@ -1,1 +1,30 @@
+import sqlalchemy
+import elixir
+from sqlalchemy.engine.url import URL
+
+
+def init(**config):
+    url = config.pop('url', {})
+    url.setdefault('drivername', 'mysql+oursql')
+    url.setdefault('database', 'TagOpsDB')
+    do_create = config.pop('create', False)
+
+    if do_create:
+        create_url = url.copy()
+        db_name = create_url.pop('database')
+        engine = sqlalchemy.create_engine(URL(**create_url), **config)
+        engine.execute('CREATE DATABASE IF NOT EXISTS %s' % db_name)
+
+    elixir.metadata.bind = sqlalchemy.create_engine(
+        URL(**url), **config
+    )
+
+    elixir.setup_all(create_tables=do_create)
+
+
+def destroy():
+    elixir.cleanup_all(drop_tables=True)
+    engine = elixir.metadata.bind
+    engine.execute('DROP DATABASE IF EXISTS %s' % engine.url.database)
+
 from .model import *
