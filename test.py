eabd3f56a1020f4e785014fda23c9b344daaf650
Mike Dougherty <mdougherty@tagged.com>
2014-05-21 12:27:44 -0700

    test.py Got rid of a bunch of schema gen stuff, remove elixir refs

--- test.py
+++ test.py
@@ -44,76 +44,6 @@ def test_model_and_direct_relationships(cls):
         print '\t'+rel.key+':', val
 
 
-def write_schema():
-    import sqlalchemy
-    import elixir
-
-    engine = None
-    f = None
-
-    with open('schema.mysql.txt', 'w') as f:
-        def executor(s, p=';'):
-            compiled = s.compile(dialect=engine.dialect)
-            f.write(unicode(compiled).encode('utf8') + p)
-
-        engine = sqlalchemy.create_engine(
-            'mysql+oursql://',
-            strategy='mock',
-            executor=executor
-        )
-        elixir.metadata.bind = engine
-        elixir.setup_all()
-        elixir.create_all()
-
-
-def create_environments():
-    for long in ('development', 'staging', 'production'):
-        prefix = long[0]
-        short = long[:4]
-        domain = 'example-%s.org' % short
-
-        tagopsdb.Environment.update_or_create(dict(
-            environment=long,
-            env=short,
-            domain=domain,
-            prefix=prefix
-        ))
-
-
-def create_vlans():
-    for env in tagopsdb.Environment.all():
-        tagopsdb.Vlan.update_or_create(dict(
-            name='%s-vlan' % env.env,
-            description='for %s' % env.domain,
-            environment=env,
-        ))
-
-
-def create_ganglia():
-    tagopsdb.Ganglia.update_or_create(dict(
-        cluster_name='some-ganglia-thing'
-    ))
-
-
-def create_applications():
-    prod = tagopsdb.Environment.get_by(environment='production')
-    dev = tagopsdb.Environment.get_by(environment='development')
-    stage = tagopsdb.Environment.get_by(environment='staging')
-    ganglia = tagopsdb.Ganglia.first()
-    for app_name in ('clamor', 'web', 'hubot'):
-        tagopsdb.Application.update_or_create(dict(
-            name=app_name,
-            host_base=app_name,
-            puppet_class=app_name,
-            ganglia_group_name='%s hosts' % app_name,
-            description="%s application" % app_name,
-            ganglia=ganglia,
-            production_vlan=prod.vlans[0],
-            development_vlan=dev.vlans[0],
-            staging_vlan=stage.vlans[0],
-        ))
-
-
 def test():
     proj = tagopsdb.Project(name='abuse-finder-project')
     pkg_def = tagopsdb.PackageDefinition(
@@ -144,7 +74,7 @@ def test():
 
     proj.package_definitions.append(pkg_def)
     proj.apps.append(tagopsdb.Application.first())
-    elixir.session.commit()
+    tagopsdb.Session.commit()
 
 
 if __name__ == '__main__':
@@ -160,9 +90,6 @@ if __name__ == '__main__':
             create=True,
         )
 
-        import elixir
-        # elixir.metadata.bind.echo = True
-
         create_environments()
         create_vlans()
         create_ganglia()
@@ -171,5 +98,4 @@ if __name__ == '__main__':
         # import code
         # code.interact(local=locals())
     finally:
-        elixir.metadata.bind.echo = False
         tagopsdb.destroy()
