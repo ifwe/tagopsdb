476e61c8757c279a2bc8bc9970d56d0e55cdbe5a
Kenneth Lareau <klareau@tagged.com>
2015-02-19 15:24:49 -0800

    Add 'jmx_port' column to 'jmx_attributes' table

--- tagopsdb/model/jmx_attribute.py
+++ tagopsdb/model/jmx_attribute.py
@@ -9,4 +9,9 @@ class JmxAttribute(Base):
     id = Column(u'jmx_attribute_id', INTEGER(), primary_key=True)
     obj = Column(String(length=300), nullable=False)
     attr = Column(String(length=300), nullable=False)
+    jmx_port = Column(
+        INTEGER(unsigned=True, display_width=5),
+        nullable=False,
+        server_default='9004'
+    )
     g_group_name = Column(u'GgroupName', String(length=25))
