8f1d678269fc61a9c831a6d26575836239380bd7
Karandeep Nagra <knagra@ifwe.co>
2015-10-05 14:42:07 -0700

    Move package_id from deployments to (host|app)_deployments

---
+++ alembic/versions/2015-10-05-13-44_move_package_id_from_deployments_to_.py
@@ -0,0 +1,55 @@
+"""Move package_id from deployments to (host|tier)_deployments
+
+Revision ID: 2b29596623f7
+Revises: 1e725eb01a1f
+Create Date: 2015-10-05 13:44:11.645008
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2b29596623f7'
+down_revision = '1e725eb01a1f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column(
+        'host_deployments',
+        sa.Column(
+            'package_id',
+            sa.Integer,
+            sa.ForeignKey('packages.package_id', ondelete='cascade'),
+            nullable=False,
+            info={'after': 'host_id'},
+        ),
+    )
+
+    op.execute(
+        'update host_deployments join deployments on '
+        'host_deployments.DeploymentID = deployments.DeploymentID '
+        'set host_deployments.package_id = deployments.package_id'
+    )
+
+    op.add_column(
+        'app_deployments',
+        sa.Column(
+            'package_id',
+            sa.Integer,
+            sa.ForeignKey('packages.package_id', ondelete='cascade'),
+            nullable=False,
+            info={'after': 'app_id'},
+        ),
+    )
+
+    op.execute(
+        'update app_deployments join deployments on '
+        'app_deployments.DeploymentID = deployments.DeploymentID '
+        'set app_deployments.package_id = deployments.package_id'
+    )
+
+
+def downgrade():
+    op.drop_column('host_deployments', 'package_id')
+    op.drop_column('app_deployments', 'package_id')
