b67ebe221e73839bf31f64d71604f16a59b82c9c
Kenneth Lareau <klareau@tagged.com>
2016-01-14 15:31:08 -0800

    Update alembic and merge alembic changes from master branch

---
+++ alembic/versions/2016-01-14-15-26_merge_master_branch_into_tds-120_.py
@@ -0,0 +1,22 @@
+"""merge master branch into tds-120 branch
+
+Revision ID: b4c9f4a3e166
+Revises: ('13f87d9ec845', '10aa1611b456')
+Create Date: 2016-01-14 15:26:33.944512
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b4c9f4a3e166'
+down_revision = ('13f87d9ec845', '10aa1611b456')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
