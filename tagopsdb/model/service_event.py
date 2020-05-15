742570df0b3265d82c8c9f633e2547f922d4fa07
Corey Hickey <chickey@tagged.com>
2020-05-15 11:22:10 -0700

    SRE-1148: catch up model to serviceEvent changes

--- tagopsdb/model/service_event.py
+++ tagopsdb/model/service_event.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sqlalchemy import ForeignKey
-from sqlalchemy.dialects.mysql import INTEGER, TEXT, TIMESTAMP
+from sqlalchemy import ForeignKey, Enum
+from sqlalchemy.dialects.mysql import INTEGER, SMALLINT, TEXT, TIMESTAMP
 from sqlalchemy.sql.expression import func
 
 from .meta import Base, Column, String
@@ -28,6 +28,17 @@ class ServiceEvent(Base):
         INTEGER(),
         ForeignKey('hosts.HostID', ondelete='cascade')
     )
+    app_id = Column(
+        u'app_id',
+        SMALLINT(),
+        ForeignKey('app_definitions.AppID'),
+    )
+    # This is deliberately _not_ linked to the hosts table; we want to record
+    # the hostname at the time of the event and not have it change later.
+    hostname = Column(String(length=30))
+    username = Column(String(length=30))
+    control_host = Column(String(length=30))
+    event = Column(Enum(u'RMA', u'commission', u'decommission'))
     service_status = Column(u'serviceStatus', String(length=100))
     comments = Column(TEXT())
     service_date = Column(
